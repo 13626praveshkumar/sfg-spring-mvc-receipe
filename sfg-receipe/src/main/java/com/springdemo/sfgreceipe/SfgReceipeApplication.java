c3fd192ec107da845cc291df787ce8acdb27ad23
13626praveshkumar <praveshkumar1347906@gmail.com>
2022-05-14 00:31:12 +0530

    Initialize Project.Closes #1

---
+++ sfg-receipe/src/main/java/com/springdemo/sfgreceipe/SfgReceipeApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.sfgreceipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgReceipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgReceipeApplication.class, args);
+	}
+
+}
---
+++ sfg-receipe/src/test/java/com/springdemo/sfgreceipe/SfgReceipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.sfgreceipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgReceipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
